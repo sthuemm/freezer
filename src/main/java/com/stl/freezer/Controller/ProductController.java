911d8176f36943026ad3916bd26a2ece7312b704
Sebastian Thümmel <sethuemm@gmx.de>
2017-09-02 22:30:30 +0200

    enabled cors

--- src/main/java/com/stl/freezer/Controller/ProductController.java
+++ src/main/java/com/stl/freezer/Controller/ProductController.java
@@ -5,15 +5,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by Basti on 01.09.2017.
  */
 
 @Controller
+@CrossOrigin(origins = "*")
 @RequestMapping(path="/product")
 public class ProductController {
 
